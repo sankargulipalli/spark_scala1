f8a3a26239212026c1870e59403a7e53ed43b560
shankar <shankar@sankar>
2019-06-13 14:14:50 +0530

    Olympix data analyze

--- traning2/src/main/scala/org/spark/training2/Olympix.scala
+++ traning2/src/main/scala/org/spark/training2/Olympix.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.RelationalGroupedDataset
 
+
 object Olympix {
 
   def main(args: Array[String]): Unit = {
@@ -28,18 +29,23 @@ object Olympix {
 
     //val raw_source1 = ss.read.format("json").schema(schema).option("header", "true").load("F:\\MY_LEARNING\\SCALA\\olympic.json")
     
-  val raw_source1 = ss.read.format("json").option("header", "true").load("F:\\MY_LEARNING\\SCALA\\olympic.json")//working
+  val raw_source1 = ss.read.format("json").option("header", "true").option("inferSchema",true) load("F:\\MY_LEARNING\\SCALA\\olympic.json")//working
   //  val raw_source1 = ss.read.format("csv").option("header", "true").option("delimiter", ",").load("F:\\MY_LEARNING\\SCALA\\olympix_data_sample.csv")
-    
+    import ss.implicits._
   //  raw_source1.write.parquet("wf_parq.parquet") //working good
      raw_source1.write.mode("overwrite").parquet("wf_parq.parquet")
     val rf_parq = ss.read.parquet("wf_parq.parquet")
     //val p_s_df =  rf_parq.filter(rf_parq("country")==="Russia") //working
    // p_s_df.show()
-    //val cty_rank = rf_parq.groupBy(rf_parq("country")).sum("total").orderBy(desc("sum(total)"))//working
+    //val cty_rank = rf_parq.groupBy(rf_parq("country")).sum("total").orderBy(desc("sum(total)"))//not working
     //val cty_rank = rf_parq.groupBy(rf_parq("country")).sum("total").agg(max("sum(total)")).alias("Rank")
-    val ctry_swim = rf_parq.filter(rf_parq("sport")==="Swimming").groupBy(rf_parq("country")).sum("total").orderBy(("sum(total)"))
-     ctry_swim.show()
+    val ctry_swim = rf_parq.filter(rf_parq("sport")==="Swimming").groupBy(rf_parq("country")).sum("total").orderBy(("sum(total)")) //working
+  //  val df_ds = rf_parq.as[(String,Int,String,Int,String,Int,Int,Int,Int,Int)] //not working
+    
+    val usa_total = rf_parq.filter(rf_parq("country")==="United States").groupBy(rf_parq("year")).sum("total")
+     usa_total.show()
+   
+     //ctry_swim.show()
     
     //cty_rank.show()
        
