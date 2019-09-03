ef3b631c92c220ec816824d33cc1d22cc01642df
shankar <shankar@sankar>
2019-09-03 09:29:14 +0530

    adding function calling

--- traning2/src/main/scala/org/spark/training2/Scala_loop.scala
+++ traning2/src/main/scala/org/spark/training2/Scala_loop.scala
@@ -1,14 +1,35 @@
 package org.spark.training2
 
 object Scala_loop {
+   def sample_fun()
+   {
+     print("this is simple function calling!!")
+     
+   }
+  
   def main(args: Array[String]): Unit = {
+    sample_fun()
+    
     val lst = List(1, 2, 3, 4, 5)
     for (i <- lst) {
-      print(i + "\n")
+   //   print(i + "\n")
     }
     
     val result = for(i <- lst;if i<4)yield {  i* i} 
     print(result)
+    
+    //print("Adding case statment")
+    
+val age = 50
+age match {
+case 20 =>print(age)
+case 30 =>print(age)
+case 40 =>print(age)
+case 50 =>print(age)
+case _  =>print("default")
+
+}
+    
   }
 
 }
