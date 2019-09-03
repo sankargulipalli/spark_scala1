4eb00da7a7370395b7c850432d6ed7279d02f709
shankar <shankar@sankar>
2019-09-03 09:50:56 +0530

    adding function with parameter passing

--- traning2/src/main/scala/org/spark/training2/Scala_loop.scala
+++ traning2/src/main/scala/org/spark/training2/Scala_loop.scala
@@ -1,15 +1,33 @@
 package org.spark.training2
 
 object Scala_loop {
-   def sample_fun()
+  
+     def sample_fun()
    {
      print("this is simple function calling!!")
      
    }
-  
+  object inner
+  {
+    def inner_fun()
+    {
+      print("this is simple fucntion in inner object")
+    }
+    
+    def para_fun(x:Int,y:Int):Int=
+    {
+      return x+y
+      
+    }
+    
+  }
+     
+     
+     
   def main(args: Array[String]): Unit = {
     sample_fun()
-    
+    inner.inner_fun() //calling the function which is in another object
+    print("\nthe sum of the values x +y is :" + inner.para_fun(10,20))
     val lst = List(1, 2, 3, 4, 5)
     for (i <- lst) {
    //   print(i + "\n")
